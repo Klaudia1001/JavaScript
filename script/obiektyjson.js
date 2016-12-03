6f80aa1cbe1fa985f5263fc7bf425c2720c51d47
Klaudia Przespolewska <klaudia1000@onet.eu>
2016-12-03 17:00:28 +0100

    json

---
+++ script/obiektyjson.js
@@ -0,0 +1,11 @@
+'use script';
+
+var jsonLaptopy = {
+    "laptopki" : [
+        {nazwa: "Acer", kolor: "czarny", stan: "nowy", procesor: "i5"},
+        {nazwa: "Lenovo", kolor: "niebieski", stan: "używany", procesor: "i4"},
+        {nazwa: "Samsung", kolor: "czerwony", stan: "nowy", procesor: "i5"}
+    ]
+}
+
+console.log(jsonLaptopy);
